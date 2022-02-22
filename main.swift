6c96a406c2786c1b766d0c54aff0907ba3ab37a7
k0id <shvilpo@gmail.com>
2022-02-22 08:27:22 +0000

    (ADD) function functions paraneters and returns

--- main.swift
+++ main.swift
@@ -1,6 +1,45 @@
 
-var firstForLoop = 0
-for i in 0..<4 {
-    firstForLoop += i
+print(firstFunc(st:"Hellolious", num: 10))
+
+
+func firstFunc(st: String, num: Int) -> String {
+  return "\(st)-\(num)"
 }
-print(firstForLoop)
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+  var min=scores[0]
+  var max=scores[0]
+  var sum=0
+  for score in scores {
+    if score>max {max=score}
+    else if score<min {min=score}
+    sum+=score
+  }
+  return (min, max, sum)
+}
+
+let stats = calculateStatistics(scores: [100, 36, -87, 4*12, 67, -1000])
+print("min:\(stats.min), max:\(stats.1), sum:\(stats.sum)");
+
+func makeIncrementer() -> ((Int) -> Int) {
+  func addOne(number: Int) -> Int {
+    return number + 1
+  }
+  return addOne
+}
+var increment = makeIncrementer()
+increment(7)
+
+func hasAnyMatches(list: [Int], condition: (Int)-> Bool ) -> Bool {
+  for item in list {
+    if condition (item) {
+      return true
+    }
+  }
+  return false
+}
+func lessThan10 (item: Int) -> Bool {
+  return item < 10
+}
+var numbers = [10,15,181, 31, 54, 9]
+print (hasAnyMatches(list: numbers, condition: lessThan10))
